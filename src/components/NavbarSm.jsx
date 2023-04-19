9e77d48e589170552f3b8b32b983c7dd93e6aa1e
Ronalis Saputra <macbookair@MacBook-Air-Macbook-2.local>
2023-04-20 02:22:19 +0800

    updated navbar

--- src/components/NavbarSm.jsx
+++ src/components/NavbarSm.jsx
@@ -7,13 +7,13 @@ const NavbarSm = () => {
   const [menu, setMenu] = useState(false);
   return (
     <>
-      <div className="flex lg:hidden overflow-hidden top-0 sticky px-2 bg-slate-400 w-full h-14 items-center justify-between">
+      <div className="flex lg:hidden overflow-hidden top-0 sticky px-2 bg-indigo-700 w-full h-14 items-center justify-between">
         <div>
-          <p className="text-3xl text-indigo-700">HollaShop</p>
+          <p className="text-2xl text-slate-50">HollaShop</p>
         </div>
         <div>
           <div onClick={() => setMenu(!menu)}>
-            <HiMenuAlt3 className="text-3xl cursor-pointer" />
+            <HiMenuAlt3 className="text-3xl cursor-pointer text-slate-50" />
           </div>
           <div
             className={`w-full h-20 bg-slate-100 min-h-screen translate transition-all duration-300 transform top-0 ${
@@ -22,12 +22,12 @@ const NavbarSm = () => {
           >
             <div className="m-2">
               <div className="flex justify-between">
-                <p className="text-3xl">Holla Shop</p>
+                <p className="text-2xl">Holla Shop</p>
                 <div onClick={() => setMenu(!menu)}>
                   <GrFormClose className="text-3xl" />
                 </div>
               </div>
-              <div className="mt-20 flex flex-col space-y-2 items-center justify-center">
+              <div className="mt-20 flex flex-col space-y-6 items-center justify-center">
                 <Link to="/" className="text-2xl">
                   Beranda
                 </Link>
